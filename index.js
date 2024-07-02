c62e094d0e4f5319dba7de1c47042fb274777eaf
William Powell <william.powell@astrazeneca.com>
2024-07-02 14:28:53 +0100

    Adding monthlyTerm variable

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function loanCalculator() {
     loanAmount = document.getElementById("loan-amount").value;
   
     interestRate = document.getElementById("interest-rate").value;
+
+    monthlyTerm = document.getElementById("months-to-pay").value;
   
   }
